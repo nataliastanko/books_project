f893474e382581200d7e491ff77b61fe7f0f15db
beata <beata@baningo.com>
2017-12-06 15:40:33 +0100

    install knp menu bundle

---
+++ src/AppBundle/Menu/Builder.php
@@ -0,0 +1,16 @@
+<?php
+namespace AppBundle\Menu;
+
+use Knp\Menu\MenuFactory;
+
+class Builder
+{
+    public function mainMenu(MenuFactory $factory, array $options)
+    {
+        $menu = $factory->createItem('root');
+        $menu->setChildrenAttribute('class', 'nav navbar-nav');
+        $menu->addChild('Home', ['uri' => 'homepage']);
+
+        return $menu;
+    }
+}
